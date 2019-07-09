578b08568455f1e2a14eb865de9f80cdc6d01094
Your Name <you@example.com>
2019-07-09 17:50:40 +0800

    ch4 dynamic sql

--- src/main/java/com/james/mybatis/mapper/UserMapper.java
+++ src/main/java/com/james/mybatis/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.james.mybatis.mapper;
 
 import com.james.mybatis.entity.SysRole;
 import com.james.mybatis.entity.SysUser;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,4 +14,16 @@ public interface UserMapper {
     List<SysUser> selectAll();
 
     List<SysRole> selectRoleByUserId(Long id);
+
+    int insert(SysUser sysUser);
+
+    int insert1(SysUser sysUser);
+
+    int updateById(SysUser sysUser);
+
+    /*多个接口参数查询时，可以使用param注解*/
+    List<SysRole> selectRoleByUserIdAndEnabled(@Param("userId") long userId,@Param("enabled")Integer enabled);
+
+    /*动态拼接sql语句测试*/
+    List<SysUser> selectByUser(SysUser sysUser);
 }
--- src/test/java/com/james/mybatis/mapper/UserMapperTest.java
+++ src/test/java/com/james/mybatis/mapper/UserMapperTest.java
@@ -11,9 +11,11 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.jws.soap.SOAPBinding;
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.management.OperatingSystemMXBean;
+import java.util.Date;
 import java.util.List;
 
 
@@ -70,6 +72,158 @@ public class UserMapperTest extends BaseMapperTest {
         }finally {
             sqlSession.close();
         }
+    }
+
+    @Test
+    public void testInsert(){
+
+        SqlSession sqlSession=getSqlSession();
+
+        try{
+            UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+
+            SysUser sysUser=new SysUser();
+            sysUser.setUserName("james");
+            sysUser.setUserPassword("123456");
+            sysUser.setUserEmail("291387044@qq.com");
+            sysUser.setUserInfo("good man");
+            sysUser.setHeadImg(new byte[]{1,2,3,4});
+            sysUser.setCreateTime(new Date());
+
+            //result为执行sql影响的行数
+            int result=userMapper.insert(sysUser);
+
+
+            //只插入了1条数据
+            Assert.assertEquals(1,result);
+            //id为空，没有给id赋值，并且没有配置给id回写
+            Assert.assertNull(sysUser.getId());
+
+        }finally {
+            //这里选择不提交
+            //sqlSessionFactory.openSession默认不自动提交
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+
+
+    }
+
+    @Test
+    public void testSelectRoleByUserIdAndEnabled(){
+        SqlSession sqlSession=getSqlSession();
+        try {
+            UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+            List<SysRole> sysRoles=userMapper.selectRoleByUserIdAndEnabled(1001l,1);
+
+            //会找到不止一个可用角色
+            Assert.assertTrue(sysRoles.size()>0);
+
+
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testSelectByUser(){
+        SqlSession sqlSession=getSqlSession();
+
+        try{
+            UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+
+            SysUser queryUser=new SysUser();
+
+            queryUser.setUserName("ad");
+
+
+            List<SysUser> sysUserList1=userMapper.selectByUser(queryUser);
+
+            //通过用户名模糊查询
+            Assert.assertTrue(sysUserList1.size()>0);
+
+            queryUser.setUserName(null);
+            queryUser.setUserEmail("admin@mybatis.tk");
+
+            sysUserList1=userMapper.selectByUser(queryUser);
+
+            //通过邮箱精确查询
+            Assert.assertTrue(sysUserList1.size()>0);
+
+            queryUser.setUserName("ad");
+            queryUser.setUserEmail("test@mybatis.tk");
+
+            sysUserList1=userMapper.selectByUser(queryUser);
+
+            //通过用户名模糊查询,邮箱名精确查询,
+            Assert.assertTrue(sysUserList1.size()==0);
+
+
+        }finally {
+            sqlSession.close();
+        }
+
+    }
+
+    @Test
+    /*
+    *测试使用JDBC返回自增式主键，适用于mysql,sql server
+    * */
+    public void testInsert1(){
+        SqlSession sqlSession=getSqlSession();
+        try{
+
+            UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+
+            SysUser sysUser=new SysUser();
+            sysUser.setUserName("lbj");
+            sysUser.setUserPassword("123456");
+            sysUser.setUserEmail("291387044@tk");
+            sysUser.setUserInfo("bad boy");
+            sysUser.setHeadImg(new byte[]{1,2,3,4});
+            sysUser.setCreateTime(new Date());
+
+            int result=userMapper.insert1(sysUser);
+
+            Assert.assertEquals(1,result);
+
+            Assert.assertNotNull(sysUser.getId());
+
+        }finally {
+
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testUpdateById(){
+
+        SqlSession sqlSession=getSqlSession();
+       try{
+
+           UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+           SysUser sysUser=userMapper.selectById(1l);
+
+           Assert.assertEquals("admin",sysUser.getUserName());
+
+           Assert.assertEquals("admin@mybatis.tk",sysUser.getUserEmail());
+
+           sysUser.setUserEmail("admin_test@mybatis.tk");
+
+           int result=userMapper.updateById(sysUser);
+
+           Assert.assertEquals(1,result);
+
+           Assert.assertEquals("admin_test@mybatis.tk",sysUser.getUserEmail());
+
+
+       }finally {
+
+            sqlSession.rollback();
+            sqlSession.close();
+       }
+
     }
 
     private void printUserList(List<SysUser> sysUserList){
